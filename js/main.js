ab4c8c770c323a7b06d35a1b15c09ca5c02704ae
Jay <Jayrmontana@yahoo.com>
2015-12-17 12:37:01 -0600

    Final Project 12-17-15

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+	
+	/* Your JS here */
+
+	// After creating your functions below, if they need to run on page load put them here
+	yourFunction();
+	$(".fancybox").fancybox();
+
+});
+
+// If you need to write functions create them down here
+function yourFunction() {
+
+}
